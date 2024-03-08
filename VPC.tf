775f1ae41a7c90968bd89c2ff4dfe371af8f0fec
AftabMulani11 <aftabmulani001@gmail.com>
2024-03-08 19:53:14 +0530

    updated VPC with Public dns support

--- VPC.tf
+++ VPC.tf
@@ -2,6 +2,8 @@
 resource "aws_vpc" "Terraform_Created" {
   cidr_block       = "10.10.0.0/16"
   instance_tenancy = "default"
+  enable_dns_support = true
+  enable_dns_hostnames = true
   tags = {
     Name = "Terraform_Created_VPC"
   }
