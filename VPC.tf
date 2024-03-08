4e160cba7928d60b467850ee54a521b145d64db4
AftabMulani11 <aftabmulani001@gmail.com>
2024-03-08 19:54:17 +0530

    Maped Public IP on Launch

--- VPC.tf
+++ VPC.tf
@@ -14,6 +14,7 @@ resource "aws_subnet" "Terraform_Created_Subnet_1a" {
   vpc_id            = aws_vpc.Terraform_Created.id
   cidr_block        = "10.10.1.0/24"
   availability_zone = "ap-south-1a"
+  map_public_ip_on_launch = true
   tags = {
     Name = "Terraform_Created_Subnet"
   }
